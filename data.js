9cbfd79a53b86a37f825418c6f3f6a9a4fa14f42
DocDuttle <100231582+DocDuttle@users.noreply.github.com>
2025-07-31 20:35:26 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const lebensmittel = {
+  hauptgang: [
+    "Bolognese", "Tomatensauce", "Käsesauce", "Pesto", "Schnitzel", "Fischstäbchen",
+    "Chicken Nuggets", "Bratwurst", "Pizza", "Currywurst", "Wiener Würstchen",
+    "Hackbällchen", "Pfannkuchen", "Rührei", "Spiegelei", "Omelett", "Frikadellen",
+    "Kaiserschmarrn", "Gemüsesuppe", "Tomatensuppe", "Kartoffelsuppe", "Gulaschsuppe",
+    "Lasagne", "Maultaschen", "Linsensuppe", "Chili sin Carne", "Reis mit Sojasauce",
+    "Rahmspinat mit Ei", "Rahmschnitzel", "Käsetoast"
+  ],
+  beilage: [
+    "Spaghetti", "Farfalle", "Gnocchi", "Tortellini", "Pommes", "Kartoffelpüree",
+    "Kartoffeln", "Reis", "Nudeln klassisch", "Couscous", "Polenta", "Butterbrot",
+    "Weißbrot", "Vollkornbrot", "Brötchen", "Toast", "Rösti", "Kroketten", "Ofenkartoffeln",
+    "Salzkartoffeln", "Grießnocken", "Kartoffelknödel", "Bulgur", "Maiskolben",
+    "Süßkartoffelpommes", "Knoblauchbrot", "Pitabrot", "Baguette", "Naan", "Blätterteigstangen"
+  ],
+  gemuese: [
+    "Brokkoli", "Karotten", "Gurkensticks", "Bohnen", "Erbsen", "Mais", "Spinat",
+    "Zucchini", "Paprika", "Kohlrabi", "Tomaten", "Blumenkohl", "Rote Bete", "Radieschen",
+    "Lauch", "Fenchel", "Champignons", "Spargel", "Edamame", "Rosenkohl", "Zwiebeln",
+    "Knoblauch", "Avocado", "Kürbis", "Sellerie", "Mangold", "Chinakohl", "Linsen",
+    "Sauerkraut", "Maisgemüse"
+  ],
+  dessert: [
+    "Pudding", "Vanilleeis", "Früchtequark", "Joghurt", "Apfelmus", "Milchreis",
+    "Wackelpudding", "Erdbeeren mit Sahne", "Banane", "Kakao", "Schokomousse", "Pfirsichhälften",
+    "Apfelscheiben", "Eis am Stiel", "Götterspeise", "Früchtebecher", "Bananenshake",
+    "Kinderkeks", "Mini-Donut", "Schokobrötchen", "Kaiserschmarrn", "Rosinenbrot",
+    "Beerenmix", "Obstsalat", "Grießbrei", "Marillenknödel", "Pancakes", "Vanillekipferl",
+    "Nutella-Crêpe", "Birne Helene"
+  ]
+};
